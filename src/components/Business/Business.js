deb5489052d2d2a09774690ccfed0231026ed55c
Stefan Dobre <stefan.g.dobre@gmail.com>
2020-08-04 15:54:40 +0300

    bugs fixed, app works

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -9,14 +9,15 @@ class Business extends React.Component {
       <div className="Business">
         <div className="image-container">
           <img
-            src="https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg"
+            src={this.props.business.imageSrc}
             alt=""
           />
         </div>
-        <h2>MarginOtto Pizzeria</h2>
+        <h2>{this.props.business.name}</h2>
         <div className="Business-information">
           <div className="Business-address">
             <p>{this.props.business.name}</p>
+            <p>{this.props.business.address}</p>
             <p>{this.props.business.city}</p>
             <p>{this.props.business.state+this.props.business.zipCode}</p>
           </div>
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -15,16 +15,21 @@ const Yelp = {
         return response.json();
       })
       .then((jsonResponse) => {
+        console.log(jsonResponse);
         if (jsonResponse.businesses) {
-          jsonResponse.businesses.map((business) => {
-            return { id: business.id, imageSrc: business.image_url, name:'',
-                address: '',
-                city: '',
-                state: '',
-                zipCode: '',
-                category: '',
-                rating: '',
-                reviewCount: '' };
+          return jsonResponse.businesses.map((business) => {
+            return {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.location.address1,
+              city: business.location.city,
+              state:  business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories.title,
+              rating: business.rating, 
+              reviewCount: business.review_count
+            };
           });
         }
       });
